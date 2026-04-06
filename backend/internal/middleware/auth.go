1d001de2b052dce183aa0c17e8248e01642361a0
Katie Sutherland <katiesuth4@gmail.com>
2026-04-05 21:35:18 -0400

    removed debug statements

--- backend/internal/middleware/auth.go
+++ backend/internal/middleware/auth.go
@@ -2,7 +2,6 @@ package middleware
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strings"
 
@@ -19,7 +18,6 @@ func Auth(jwtSecret []byte) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
-			log.Printf("unauthorized because auth header missing")
 			c.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
@@ -27,7 +25,6 @@ func Auth(jwtSecret []byte) gin.HandlerFunc {
 		// Header must be "Bearer <token>"
 		parts := strings.SplitN(authHeader, " ", 2)
 		if len(parts) != 2 || parts[0] != "Bearer" {
-			log.Printf("unauthorized because bearer incorrect")
 			c.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
@@ -39,14 +36,12 @@ func Auth(jwtSecret []byte) gin.HandlerFunc {
 			return jwtSecret, nil
 		})
 		if err != nil || !token.Valid {
-			log.Printf("unauthorized because couldn't parse token: %v", err)
 			c.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 
 		claims, ok := token.Claims.(*Claims)
 		if !ok {
-			log.Printf("unauthorized because claims bad")
 			c.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
